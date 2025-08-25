364833152fecd4faf4e65c9f5dd7ba0266cd72e7
SarahSu92 <sarah.sundberg@outlook.com>
2025-08-25 13:55:56 +0200

    (Feat):added placeholder image

--- src/pages/AnimalPage.tsx
+++ src/pages/AnimalPage.tsx
@@ -1,36 +1,44 @@
-import { useEffect, useState } from "react"
-import type { Animals } from "../models/Animals"
-import'../sass/Animals.scss';
+import { useEffect, useState } from 'react';
+import type { Animals } from '../models/Animals';
+import '../sass/Animals.scss';
+
 
 export const Animal = () => {
-    const [animals, setAnimals] = useState<Animals[]>([]);
+  const [animals, setAnimals] = useState<Animals[]>([]);
 
-    useEffect(() => {
-        const getAnimals = async () => {
-            const response = await fetch(
-                "https://animals.azurewebsites.net/api/animals"
-            );
-            const animals = await response.json();
-            setAnimals(animals);
-        }
+  useEffect(() => {
+    const getAnimals = async () => {
+      const response = await fetch(
+        'https://animals.azurewebsites.net/api/animals'
+      );
+      const animals = await response.json();
+      setAnimals(animals);
+    };
 
-        if (animals.length > 0) return;
-        getAnimals();
-    })
+    if (animals.length > 0) return;
+    getAnimals();
+  });
 
+  return (
+    <div className="container">
+      {animals.map((a) => (
+        <div className="animal-frame" key={a.id}>
+          <h2>{a.name}</h2>
 
-    return (
-        <div className="container">
-            {animals.map((a) => (
-                <div className="animal-frame" key={a.id}>
-                    <h2>{a.name}</h2>
-                    <img src={a.imageUrl} alt={a.shortDescription} />
-                    <p>{a.latinName}</p>
-                    <p>{a.yearOfBirth}</p>
-                    <h3>{a.longDescription}</h3>
-                    </div>
-
-            ))}
+          <img
+            src={a.imageUrl}
+            alt={`Bild på ${a.name}, som heter ${a.latinName} på latin. ${a.shortDescription}`}
+            onError={(e) => {
+              e.currentTarget.src = 'public/No-Image-Placeholder.svg'; 
+            }}
+          />
+          <p>{a.shortDescription}</p>
+          <p>{a.latinName}</p>
+          <p>{a.yearOfBirth}</p>
+          <h3>{a.longDescription}</h3>
+          <button>Mata</button>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import'../sass/HeroSection.scss';
+
 export const Home = () => {
     return<>Home</>
 }
