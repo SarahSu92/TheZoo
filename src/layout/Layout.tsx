9d640aa38aab1b1fb1cd966c67e1556ca719c512
SarahSu92 <sarah.sundberg@outlook.com>
2025-08-25 10:31:05 +0200

    (feat): adjusted color theme

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router';
 import './Layout.scss';
 import { Navigation } from '../navigation/Navigation';
+import { Footer } from '../footer/Footer';
 
 export const Layout = () => {
     return<>
@@ -11,7 +12,7 @@ export const Layout = () => {
         <Outlet />
     </main>
     <footer>
-        
+        <Footer />
     </footer>
     </>
 }
