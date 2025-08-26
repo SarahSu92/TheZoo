996c7a767e8011fccdd23a993cbbf9293279c0b8
SarahSu92 <sarah.sundberg@outlook.com>
2025-08-26 22:16:57 +0200

    (feat). added content in herosection

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,12 @@
 import'../sass/HeroSection.scss';
+import '../sass/HeroSection.scss';
 
 export const Home = () => {
-    return<>Home</>
+    return(
+    <div className='herosection'>
+        <img className='heroimage' src='/zoo-animals.jpg' alt='Djurpark med apor, giraffer och lejon.' />
+        <h1>Välkommen till Zoo sidan</h1>
+
+    </div>
+    );
 }
