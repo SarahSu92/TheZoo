91e8206337d7fa5f14b92f86e225772e6e79d729
SarahSu92 <sarah.sundberg@outlook.com>
2025-08-26 22:04:28 +0200

    (feat). added content in footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,7 +1,26 @@
 import './footer.scss';
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
 
 export const Footer = () => {
-    return<>
-    Footer
-    </>
-}
+    return(
+    <footer>
+     <ul>
+        <li>
+            Kontakta oss: 00-000 00 00 <br />
+            Adress: Zoostigen 25
+            111 11 Borås
+        </li>
+         <li>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebookF size={24} />
+          </a>
+        </li>
+         <li>
+           <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram size={24} />
+          </a>
+        </li>
+     </ul>
+    </footer>
+    );
+};
