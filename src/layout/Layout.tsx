c2c89b0aef9114062eaa011043986c20e90748b1
SarahSu92 <sarah.sundberg@outlook.com>
2025-08-27 14:03:15 +0200

    (refactor): layout with localstorage

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,13 +2,20 @@ import { Outlet } from 'react-router';
 import './Layout.scss';
 import { Navigation } from '../navigation/Navigation';
 import { Footer } from '../footer/Footer';
-import { useReducer } from 'react';
-import { animalReducer } from '../reducers/AnimalReducer';
+import { useEffect, useReducer } from 'react';
+import { animalReducer, type AnimalState } from '../reducers/AnimalReducer';
 import { AnimalContext } from '../context/AnimalContext';
 
 export const Layout = () => {
-  const [state, dispatch] = useReducer(animalReducer, { animals: []});
+  const [state, dispatch] = useReducer(
+    animalReducer, JSON.parse(localStorage.getItem("animals") || '{"animals": []}') as AnimalState
+  );
 
+  useEffect(() => {
+    localStorage.setItem("animals", JSON.stringify(state))
+  }, [state]);
+  
+  
   return (
     <AnimalContext.Provider value={{state, dispatch}}>
       <header>
