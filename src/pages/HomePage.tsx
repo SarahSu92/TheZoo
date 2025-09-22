3979ff4677d96e305a1cfaa4871a78d539e26628
SarahSu92 <sarah.sundberg@outlook.com>
2025-09-22 15:13:14 +0200

    (feat). changed path to image

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ export const Home = () => {
       <div className="herosection">
         <img
           className="heroimage"
-          src="/zoo-animals.jpg"
+          src={`${import.meta.env.BASE_URL}zoo-animals.jpg`}
           alt="Djurpark med apor, giraffer och lejon."
         />
         <h1 className="heroh1">Välkommen till Zoo sidan</h1>
